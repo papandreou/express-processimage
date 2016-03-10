8ec2b65d16844c1a6bad1d6a5f4f978ff0347e9d
Andreas Lind <andreas@one.com>
2016-03-10 14:37:32 +0100

    Make sure that more of the filterInfo properties come out correctly when the source metadata is not available immediately.

---
+++ test/getFilterInfosAndTargetContentTypeFromQueryString.js
@@ -0,0 +1,27 @@
+/*global describe, it*/
+var getFilterInfosAndTargetContentTypeFromQueryString = require('../lib/getFilterInfosAndTargetContentTypeFromQueryString');
+
+var expect = require('unexpected');
+
+describe('getFilterInfosAndTargetContentTypeFromQueryString', function () {
+    it('should make the right engine choice even if the source Content-Type is not available until filterInfo.create is called', function () {
+        var sourceMetadata = {};
+        var filterInfosAndTargetContentTypeFromQueryString = getFilterInfosAndTargetContentTypeFromQueryString('resize=10,10', {
+            sourceMetadata: sourceMetadata
+        });
+
+        sourceMetadata.contentType = 'image/gif';
+
+        filterInfosAndTargetContentTypeFromQueryString.filterInfos[0].create();
+
+        expect(filterInfosAndTargetContentTypeFromQueryString, 'to satisfy', {
+            operationNames: [ 'gm' ],
+            filterInfos: {
+                0: {
+                    targetContentType: 'image/gif',
+                    operationName: 'gm'
+                }
+            }
+        });
+    });
+});
