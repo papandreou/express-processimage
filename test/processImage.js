7ef045dbd60b1bf46a11270a36c6c0ec171ffde7
Alex J Burke <alex@alexjeffburke.com>
2019-11-18 23:35:16 +0100

    Revert "Mark an additional place with output mismatch."
    This reverts commit f5ada1171a90aee69ccea7249a42751cccdf1192.

--- test/processImage.js
+++ test/processImage.js
@@ -283,7 +283,7 @@ describe('express-processimage', () => {
           body: expect.it('to have metadata satisfying', {
             size: {
               width: 500,
-              height: expect.it('to be a number') // FIXME: output should be 441
+              height: 441
             }
           })
         }));
